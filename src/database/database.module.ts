da367bd395ec917fcfd1c316c0b080523d3bc838
Serhii Yallin <SerYallin@yandex.kz>
2025-11-13 00:01:09 +0300

    Add lint fixes

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,15 +7,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-          type: 'postgres',
-          host: configService.get<string>('APP_CONFIG.db.host', 'localhost'),
-          port: configService.get<number>('APP_CONFIG.db.port', 5432),
-          username: configService.get<string>('APP_CONFIG.db.user', 'postgres'),
-          password: configService.get<string>('APP_CONFIG.db.password', 'postgres'),
-          database: configService.get<string>('APP_CONFIG.db.name', 'app_db'),
-          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-          synchronize: true,
-        })
+        type: 'postgres',
+        host: configService.get<string>('APP_CONFIG.db.host', 'localhost'),
+        port: configService.get<number>('APP_CONFIG.db.port', 5432),
+        username: configService.get<string>('APP_CONFIG.db.user', 'postgres'),
+        password: configService.get<string>(
+          'APP_CONFIG.db.password',
+          'postgres',
+        ),
+        database: configService.get<string>('APP_CONFIG.db.name', 'app_db'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
   ],
 })
