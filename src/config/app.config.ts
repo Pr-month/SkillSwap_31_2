c6cbf8d3a66d8911bedad4b796c2e7a640b118de
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-11-14 21:46:08 +0300

    feat: добавил refreshToken в config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -12,6 +12,8 @@ export const configuration = registerAs('APP_CONFIG', () => ({
   },
   secretToken: process.env.SECRET_TOKEN || 'secret_token',
   secretExpiresIn: (process.env.SECRET_EXPIRES_IN || '1h') as StringValue,
+  refreshToken: process.env.REFRESH_TOKEN || 'refresh_token',
+  refreshExpiresIn: (process.env.REFRESH_EXPIRES_IN || '7d') as StringValue,
 }));
 
 export type IConfig = ConfigType<typeof configuration>;
