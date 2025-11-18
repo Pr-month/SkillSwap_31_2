b7e31fd826cbafae51c55a2df68e4d3350f692f4
zsv <zsv@rtp3.ru>
2025-11-18 08:21:03 +0300

    feat: добавлен skill.entity

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -1,1 +1,36 @@
-export class Skill {}
+import { User } from 'src/users/entities/user.entity';
+import { Column, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToOne(() => Category, (category) => category.id)
+  parent: Category;
+
+  @OneToMany(() => Category, (category) => category.parent)
+  chuldren: Category[];
+}
+
+export class Skill {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @OneToOne(() => Category, (category) => category.id)
+  category: Category;
+
+  @Column()
+  images: string[];
+
+  @OneToOne(() => User, (user) => user.id)
+  owner: User;
+}
