34ccfdde583bed08257c9c7ed7981ff68eedac27
simonenkonikita <63547419+simonenkonikita@users.noreply.github.com>
2025-12-09 23:04:26 +0300

    v1

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -30,13 +30,13 @@ export class User {
   @Column()
   password: string;
 
-  @BeforeInsert()
-  async beforeInsert() {
-    this.password = await bcrypt.hash(
-      this.password,
-      process.env.HASH_SALT || 10,
-    );
+@BeforeInsert()
+async beforeInsert() {
+  if (this.password) {
+    const saltRounds = 10; // Фиксированное значение для простоты
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
+}
 
   @Column()
   about: string;
