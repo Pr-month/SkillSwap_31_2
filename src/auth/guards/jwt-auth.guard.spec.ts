99b6f8858e1a1a577a9f91fd20578e659381851e
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-12-21 20:57:18 +0300

    feat(jwt-auth.guard.spec): дополнительные юнит тесы

--- src/auth/guards/jwt-auth.guard.spec.ts
+++ src/auth/guards/jwt-auth.guard.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
 describe('JwtAuthGuard', () => {
   let guard: JwtAuthGuard;
@@ -38,6 +38,111 @@ describe('JwtAuthGuard', () => {
 
     expect(canActivateSpy).toHaveBeenCalledWith(mockExecutionContext);
     expect(result).toBe(true);
+
+    canActivateSpy.mockRestore();
+  });
+
+  it('должен пропустить запрос с валидным токеном', async () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: { authorization: 'Bearer valid-jwt-token' },
+      }),
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    const canActivateSpy = jest
+      .spyOn(guard, 'canActivate')
+      .mockResolvedValue(true);
+    const result = await guard.canActivate(mockExecutionContext);
+
+    expect(result).toBe(true);
+    expect(canActivateSpy).toHaveBeenCalledWith(mockExecutionContext);
+
+    canActivateSpy.mockRestore();
+  });
+
+  it('должен выбросить ошибку, если токен не передан', async () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: {}, // Нет заголовка authorization
+      }),
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    const canActivateSpy = jest
+      .spyOn(guard, 'canActivate')
+      .mockRejectedValue(new UnauthorizedException());
+
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+
+    canActivateSpy.mockRestore();
+  });
+
+  it('должен выбросить ошибку, если токен не содержит Bearer', async () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: { authorization: 'invalid-jwt-token' }, // Токен без Bearer
+      }),
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    const canActivateSpy = jest
+      .spyOn(guard, 'canActivate')
+      .mockRejectedValue(new UnauthorizedException());
+
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+
+    canActivateSpy.mockRestore();
+  });
+
+  it('должен выбросить ошибку, если токен невалидный', async () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: { authorization: 'Bearer invalid-jwt-token' }, // Невалидный токен
+      }),
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    const canActivateSpy = jest
+      .spyOn(guard, 'canActivate')
+      .mockRejectedValue(new UnauthorizedException());
+
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+
+    canActivateSpy.mockRestore();
+  });
+
+  it('должен выбросить ошибку, если токен просрочен', async () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({
+        headers: { authorization: 'Bearer expired-jwt-token' }, // Просроченный токен
+      }),
+      getHandler: jest.fn(),
+      getClass: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    const canActivateSpy = jest
+      .spyOn(guard, 'canActivate')
+      .mockRejectedValue(new UnauthorizedException());
+
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
 
     canActivateSpy.mockRestore();
   });
